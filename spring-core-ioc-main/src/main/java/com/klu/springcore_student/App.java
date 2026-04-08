b10f255a399a6d09752a8eb66b07f86b628d44dc
Ramyakomali08 <2400031599@kluniversity.in>
2026-04-08 07:50:02 +0530

    Add spring-core-ioc-main project files

---
+++ spring-core-ioc-main/src/main/java/com/klu/springcore_student/App.java
@@ -0,0 +1,10 @@
+package com.klu.springcore_student;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
